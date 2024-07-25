e49b3d5bdf99350579bef2b3765dab3116bf93e8
norakoo <nolanh76lh@gmail.com>
2024-07-25 02:06:12 +0200

    Initial commit

---
+++ src/main/java/net/norako/arkonia/Arkonia.java
@@ -0,0 +1,16 @@
+package net.norako.arkonia;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Arkonia implements ModInitializer {
+	public static final String MOD_ID = "arkonia";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/norako/arkonia/ArkoniaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.norako.arkonia;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ArkoniaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
